e08ff24278f8d9dc2cf1b5f469d6c458b2583552
Yone <sho.yone.127@gmail.com>
2026-05-26 01:52:13 +0900

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,23 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.cache.keys():
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache.keys():
+            self.cache.pop(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+
+        
+        
